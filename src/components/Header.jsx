5031098be8ef0df7e1e2e97025e22d6a6c49f929
Victor Mendonca <victor.men.dev@gmail.com>
2021-08-30 19:25:45 -0300

    Req 5

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  componentDidMount() {
+    console.log(this.props.emailInput);
+  }
+
+  render() {
+    const { emailInput } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">
+          { emailInput }
+        </p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  emailInput: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  emailInput: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
